6ac78bf87c2f962916a9012401de910b1c47e112
Philip Guo <philip@pgbovine.net>
2010-05-25 17:46:13 -0700

    updated crude visualization script

--- incpy-support-scripts/viz_pickles.py
+++ incpy-support-scripts/viz_pickles.py
@@ -8,69 +8,57 @@ import pprint
 import hashlib
 
 # render a dependencies 'object', which is a dict
-# (side effect: DESTROYS fmi in the process of rendering it)
 def render_dependencies(fmi):
-  all_keys = set(fmi.keys())
-
-  canonical_name = fmi.pop('canonical_name')
+  canonical_name = fmi['canonical_name']
   print '===', canonical_name, '==='
   print
 
-  try:
-    file_read_dependencies = fmi.pop('file_read_dependencies')
-    if file_read_dependencies:
-      print 'Files read:', ','.join(sorted(file_read_dependencies.keys()))
-      print
-  except KeyError:
-    pass
-
-  try:
-    file_write_dependencies = fmi.pop('file_write_dependencies')
-    if file_write_dependencies:
-      print 'Files written:', ','.join(sorted(file_write_dependencies.keys()))
-      print
-  except KeyError:
-    pass
-
-  try:
-    global_var_dependencies = fmi.pop('global_var_dependencies')
-    if global_var_dependencies:
-      print 'Global vars read:'
-      for global_var in sorted(global_var_dependencies.keys()):
-        print ' ', global_var, ':', repr(global_var_dependencies[global_var])
-      print
-  except KeyError:
-    pass
-
-  try:
-    called_funcs_set = fmi.pop('called_funcs_set')
-    if called_funcs_set:
-      print 'Called functions:'
-      for func_name in sorted(called_funcs_set):
+  code_deps = fmi['code_dependencies']
+  if len(code_deps) > 1:
+    print 'Called functions:'
+    for func_name in sorted(code_deps):
+      if func_name != canonical_name:
         print ' ', func_name
-      print
-  except KeyError:
-    pass
-
-  assert fmi.pop('self_code_dependency') # pop this but don't bother rendering it
-
-  # make sure we've rendered them all!
-  assert len(fmi) == 0
+    print
 
 
 # render one memoized_vals 'object', which is a dict
-# (side effect: DESTROYS fmi in the process of rendering it)
 def render_memoized_vals(memoized_vals):
   print 'Memoized vals:'
-  for e in memoized_vals:
+  for (i, e) in enumerate(memoized_vals):
+    print '  ~~~'
+    print '  Args:', e['args']
+
+    try:
+      global_var_dependencies = e['global_vars_read']
+      print '  Globals:'
+      for global_var in sorted(global_var_dependencies.keys()):
+        print '    ', global_var, ':', repr(global_var_dependencies[global_var])
+    except KeyError:
+      pass
+
     retval = e['retval']
     assert len(retval) == 1 # retval is formatted as a singleton list
     retval = retval[0]
-    print ' ', e['args'], '->', retval,' (', e['runtime_ms'], 'ms )'
+    print '  Return value:', retval,' (', e['runtime_ms'], 'ms )'
+
     if 'stdout_buf' in e:
       print '  stdout_buf:', repr(e['stdout_buf'])
     if 'stderr_buf' in e:
       print '  stderr_buf:', repr(e['stderr_buf'])
+
+    print
+    try:
+      file_read_dependencies = e['files_read']
+      print 'Files read:', ','.join(sorted(file_read_dependencies.keys()))
+    except KeyError:
+      pass
+
+    try:
+      file_write_dependencies = e['files_written']
+      print 'Files written:', ','.join(sorted(file_write_dependencies.keys()))
+    except KeyError:
+      pass
 
 
 def main(argv=None):
@@ -91,7 +79,7 @@ def main(argv=None):
     memoized_vals = cPickle.load(open(os.path.join(incpy_cache_dir, f)))
     render_dependencies(deps)
     render_memoized_vals(memoized_vals)
-    print '---'
+    print
 
   return 0
 
